2e9f86dd3375bfb654039318f5f5907e67abeb88
LenYx24 <65910507+LenYx24@users.noreply.github.com>
2023-03-08 16:30:08 +0100

    five

--- forras/js/app.js
+++ forras/js/app.js
@@ -1,9 +1,7 @@
-function click() {
+document.getElementById("btt").addEventListener("click", (e) => {
   const t = document.getElementById("p1");
   const p = document.getElementById("p2");
-  if (t.value === null || p.value === null) {
+  if (t.value === null || p.value === "") {
     alert("Válasszon ki egy pizzát a listából és írja ki a darabszámot!");
   }
-  console.log(t.value);
-  console.log(p.value);
-}
+});
